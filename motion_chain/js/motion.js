008670c6d9781f188b06d31d8ec1e465d67744cf
Zidong Liu <z.liu@insidepetroleum.com>
2019-11-05 16:26:46 -0600

    chain move

---
+++ motion_chain/js/motion.js
@@ -0,0 +1,29 @@
+function move(obj, styleName, target, endFn){
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function(){
+        curVal = getStyle(obj, styleName);
+        if(curVal == target){
+            clearInterval(obj.timer);
+            if(endFn){
+                endFn();
+            }
+        }else{
+            speed = (target - curVal)/6;
+            if(styleName == 'opacity'){
+                speed = speed>0?Math.ceil(speed * 100)/100:Math.floor(speed * 100)/100;
+                obj.style[styleName] = curVal + speed;
+            }else{
+                speed = speed>0?Math.ceil(speed):Math.floor(speed);
+                obj.style[styleName] = curVal + speed + 'px';
+            }
+        }
+    }, 30)
+}
+
+function getStyle(obj, styleName){
+    if(styleName == 'opacity'){
+        return parseFloat(getComputedStyle(obj)[styleName])
+    }else{
+        return parseInt(getComputedStyle(obj)[styleName])
+    }
+}
